842ecb25084115cb9556a0c74c5e6d6b3751f2ac
Mehmet Tekman <m.tekman@ucl.ac.uk>
2012-08-26 12:31:04 +0100

    Formatting corrections (Up to page 10)

---
+++ Code/convert.cpp
@@ -0,0 +1,25 @@
+//MAGENTA
+newimg1(i,j,0, 0) = pixels[0]; //red
+newimg1(i,j,0 ,1) = pixels[1]; //green
+newimg1(i,j,0, 2) = pixels[2]; //blue;
+//
+//BLACK IMAGE
+newimg2(i,j,0, 0) = pixels[0]/255; //red
+newimg2(i,j,0 ,1) = pixels[1]/255; //green
+newimg2(i,j,0, 2) = pixels[2]/255; //blue;
+//
+newimg3(i,j,0) = pixels[0]; //red
+newimg3(i,j,1) = pixels[1]; //green
+newimg3(i,j,2) = pixels[2]; //blue;
+//
+newimg4(i,j,0) = pixels[0]/255; //red
+newimg4(i,j,1) = pixels[1]/255; //green
+newimg4(i,j,2) = pixels[2]/255; //blue;
+//GREEN
+newimg5(i,j,32, 0) = pixels[0]; //red
+newimg5(i,j,32, 1) = pixels[1]; //green
+newimg5(i,j,32, 2) = pixels[2]; //blue;
+//BLACK IMAGE
+newimg6(i,j,32, 0) = pixels[0]/255; //red
+newimg6(i,j,32, 1) = pixels[1]/255; //green
+newimg6(i,j,32, 2) = pixels[2]/255; //blue
---
+++ Code/errors.cpp
@@ -0,0 +1,16 @@
+Nokia-N900:/home/user/MyDocs/DCIM/MISC\# convert -delay 20 -monitor timelapse-0000*.jpg movie.mpg
+load image[timelapse-000001.jpg]: 479 of 480, 100\% complete
+load image[timelapse-000002.jpg]: 479 of 480, 100\% complete
+...
+load image[timelapse-000015.jpg]: 479 of 480, 100\% complete
+mogrify image[timelapse-000015.jpg]: 9 of 10, 100\% complete
+Composite/Image[movie.mpg]: 479 of 480, 100\% complete
+..
+Composite/Image[movie.mpg]: 479 of 480, 100\% complete
+Save/Image//var/tmp[magick-XX9TmsAd7.jpg]: 479 of 480, 100\% complete
+..
+Save/Image//var/tmp[magick-XX9TmsAd63.jpg]: 479 of 480, 100\% complete
+convert: Empty input file `timelapse-000001.jpg' @ error/jpeg.c/EmitMessage/233.
+..
+convert: Empty input file `timelapse-000010.jpg' @ error/jpeg.c/EmitMessage/233.
+Nokia-N900:/home/user/MyDocs/DCIM/MISC\# 
