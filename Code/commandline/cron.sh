c22d6871d8ba188187d2771d3a6b0fdc74c79907
Mehmet Tekman <m.tekman@ucl.ac.uk>
2012-08-29 17:11:51 +0100

    Small update -- cant do cron explation for some reason

---
+++ Code/commandline/cron.sh
@@ -0,0 +1,10 @@
+# Source: http://en.wikipedia.org/wiki/cron
+*    *    *    *    *  command to be executed
+┬    ┬    ┬    ┬    ┬
+│    │    │    │    │
+│    │    │    │    │
+│    │    │    │    └───── day of week (0 - 6) (0 is Sunday, or use names)
+│    │    │    └────────── month (1 - 12)
+│    │    └─────────────── day of month (1 - 31)
+│    └──────────────────── hour (0 - 23)
+└───────────────────────── min (0 - 59)
